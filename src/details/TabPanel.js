ef0cc007b27b5cd37478083dfdbf9606dab61834
nithesh1997 <102368610+nithesh1997@users.noreply.github.com>
2022-05-27 18:36:37 +0530

    Add files via upload
    First Commit

---
+++ src/details/TabPanel.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import {useState , useEffect} from 'react'
+import axios from 'axios';
+import './table.css';
+import Cardfile from './Cardfile';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+  const [categoriesTotaldata,setCategoriesTotaldata]= useState([]);
+  const[removeDups,setremoveDups] = useState([]);
+ 
+  useEffect(() => {
+      let getData = async () => {
+          let dataGot = await axios.get("https://rcz-vam-1.herokuapp.com/api/getGenieRecordsByAllCategories")
+          let toRemoveDups = await dataGot.data.reduce((storageArray,e,index)=>{
+          if(!storageArray.includes(e.mainCategory)){
+              storageArray.push(e.mainCategory)
+          }
+          return storageArray
+          },[])
+          let toFilter = decodeURI(window.location.href.split("/")[window.location.href.split("/").length-1]);
+          setremoveDups(toRemoveDups.filter(e=>{
+           return e == toFilter
+
+          }));
+          let contentToFilter =await dataGot.data.filter(e=>{
+             return e.mainCategory == toFilter
+
+          })
+          setCategoriesTotaldata(contentToFilter)
+          
+      }
+    getData();
+
+  }, [])
+ 
+
+  const handleChange = (event, newValue) => {
+      setValue(newValue);
+  };
+
+  return (
+      <Box
+          sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: "500" }}
+      >
+          <Tabs
+              orientation="vertical"
+              variant="scrollable"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              sx={{ borderRight: 1, borderColor: 'divider' }}
+          >    
+          {removeDups.length ? removeDups.map((e,index)=>(
+              <Tab label={e} {...a11yProps(index)} />
+          )) :(<div></div>)}
+              {/* <Tab label="Item Two" {...a11yProps(1)} />
+              <Tab label="Item Three" {...a11yProps(2)} />
+              <Tab label="Item Four" {...a11yProps(3)} />
+              <Tab label="Item Five" {...a11yProps(4)} />
+              <Tab label="Item Six" {...a11yProps(5)} />
+              <Tab label="Item Seven" {...a11yProps(6)} /> */}
+          </Tabs>
+
+         
+             
+          
+         
+          <TabPanel value={value} index={0}>
+            {categoriesTotaldata.map(e=>(
+              <Cardfile jobFromData={e.job} priceFromData={e.price} ratingFromData={e.rating} detailsFromData={e.description}/>
+            ))}
+          
+          </TabPanel>
+         
+         
+      </Box>
+  );
+}
---
+++ src/home all components/Home.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import Nav from './Nav';
+import './home.css';
+import { Col, Row } from 'reactstrap';
+import Catecory from './Catecory';
+import Carouselcomponent from './carousel';
+import Homefooter from './Homefooter';
+import axios from 'axios';
+
+
+ function Home({locationDataFromApp}) {
+    const [fulldata,setFulldata]=useState([])
+
+    useEffect(()=>{
+
+        let getfulldata=async()=>{
+            let totaldata= await axios.get("https://rcz-vam-1.herokuapp.com/api/getGenieRecordsByAllCategories")
+            
+            setFulldata(await totaldata.data)
+              }
+
+console.log(fulldata)
+    getfulldata();
+   
+    },[])
+  
+    return (
+        <div>
+        <div className="home">
+        <Nav locationFromHome = {locationDataFromApp}/>
+       
+        </div>
+        <div>
+        <Catecory/>
+        </div>
+        <div>
+        <Carouselcomponent/>
+        </div>
+        <div>
+        <Homefooter/>
+        </div>
+        </div>
+    );
+
+}
+export default Home;
